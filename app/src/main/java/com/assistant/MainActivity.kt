396b96b32b932433d92d77f2da549c05cf4c527c
Aleksandr Cheprasov <cheprasov.dev@gmail.com>
2024-03-12 22:47:32 +0300

    added request permission

--- app/src/main/java/com/assistant/MainActivity.kt
+++ app/src/main/java/com/assistant/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.assistant
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.assistant.ui.theme.AssistantTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +37,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    ComposablePermission(permission = "android.permission.RECEIVE_SMS", {}, {})
                 }
             }
         }
@@ -38,6 +50,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun ComposablePermission(
+    permission: String,
+    onDenied: @Composable (requester: () -> Unit) -> Unit,
+    onGranted: @Composable () -> Unit
+) {
+    val ctx = LocalContext.current
+
+    var grantState by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                ctx,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+    if (grantState) {
+        Log.i("Main Activity", "Permission is granted")
+        onGranted()
+    } else {
+        val launcher: ManagedActivityResultLauncher<String, Boolean> =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) {
+                grantState = it
+            }
+        Log.i("Main Activity", "Permission isn't granted")
+        SideEffect {
+            launcher.launch(permission)
+        }
+//        onDenied { launcher.launch(permission) }
+    }
 }
 
 @Preview(showBackground = true)
